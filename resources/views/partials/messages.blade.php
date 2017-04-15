8cb3e51163dfc043ba0d9e8e8252fd553349adba
Thomas Krog <thomaskrog84@gmail.com>
2017-04-15 13:24:57 +0200

    Make sure  variable is set in messages template

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,9 +1,11 @@
-@foreach ($messages as $severity => $list)
-    @if (!empty($severity))
-        @foreach ($list as $message)
-            <div class="alert alert-{{ $severity }}">
-                {{ $message }}
-            </div>
-        @endforeach
-    @endif
-@endforeach
+@if(isset($messages))
+    @foreach ($messages as $severity => $list)
+        @if (!empty($severity))
+            @foreach ($list as $message)
+                <div class="alert alert-{{ $severity }}">
+                    {{ $message }}
+                </div>
+            @endforeach
+        @endif
+    @endforeach
+@endif
